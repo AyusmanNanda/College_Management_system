5ee193cafb733c744b4b7bb4330125c0f4095a48
Dibyasmita-Mohapatra <dibyasmita.m05@gmail.com>
2026-02-04 01:18:02 +0530

    Fix student login database mapping

--- src/college/student/StudentData.java
+++ src/college/student/StudentData.java
@@ -1,8 +1,36 @@
 package college.student;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import college.libs.DataBaseConnection;
+
 public class StudentData {
 
     public boolean checkPassword(String userid, String password) {
-        return userid.equals("student") && password.equals("1234");
+
+        boolean result = false;
+
+        try {
+            Connection con = DataBaseConnection.getConnection();
+
+            String sql = "SELECT * FROM students WHERE userid=? AND password=?";
+            PreparedStatement ps = con.prepareStatement(sql);
+            ps.setString(1, userid);
+            ps.setString(2, password);
+
+            ResultSet rs = ps.executeQuery();
+
+            if (rs.next()) {
+                result = true;
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return result;
     }
 }
+
